ec183bed4504434cea060a4fac920a8299bf5423
Patrik Pettersson <pettersson.pa@gmail.com>
2016-02-25 19:24:27 +0100

    added class method tests

--- spec/rbstruct_spec.rb
+++ spec/rbstruct_spec.rb
@@ -1,8 +1,27 @@
 require 'spec_helper'
 
+
+
 describe RbStruct do
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'Class methods' do
+    Header = RbStruct do
+      int :value
+      int :values, 10
+    end
+
+    subject { Header }
+    let(:instance) { subject.new }
+    it 'defines a class' do
+      expect(subject).to be_instance_of Class
+    end
+
+    describe '#size' do
+      it { expect(subject.size).to eq(11) }
+    end
+
+    describe '#bsize' do
+      it { expect(subject.bsize).to eq(4*11) }
+    end
   end
 end
